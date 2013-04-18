36a94c9c13a421e36a8491532f6a6161c32d28e3
facelessuser <faceless.shop@gmail.com>
2013-04-17 19:14:18 -0600

    -Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 RawLineEdit
 ===========
+<img src="https://dl.dropboxusercontent.com/u/342698/RawLineEdit/Example.png" border="0"/>
 
-View and edit line endings for Sublime Text
+View and edit line endings in Sublime Text.  RawLineEdit displays line endings very clearly and allows changing the line endings per line (something sublime text doesn't allow out of the box).
+
+# Usage
+Toggle current view to a "RawLineEdit" view via the command palette command ```Raw Line Edit: Toggle``` (you can setup your own keybinding or add it your context menu if choose)
+
+Using ```Enter``` key you can change Windows style line endings to Unix or use ```Shift+Enter``` to do the opposite.  Select multiple lines to change more than one line.
+
+# Settings
+```javascript
+    // Use a glyph for a visual representation
+    // for newlines
+    "use_newline_glyph": true,
+
+    // Use the raw line edit theme to allow
+    // a view that highlights only new lines
+    // and carriage returns for easy visualization.
+    // (Colors customizable via your color scheme file)
+    "use_raw_line_edit_theme": true
+```
+
+# Colorize Line Endings
+```XML
+        <dict>
+            <key>name</key>
+            <string>Raw New Line: Carriage Rerturn</string>
+            <key>scope</key>
+            <string>glyph.carriage-return</string>
+            <key>settings</key>
+            <dict>
+                <key>foreground</key>
+                <string>#66CCCC</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>name</key>
+            <string>Raw New Line: New Line Glyph</string>
+            <key>scope</key>
+            <string>glyph.new-line</string>
+            <key>settings</key>
+            <dict>
+                <key>foreground</key>
+                <string>#F2777A</string>
+            </dict>
+        </dict>
+```
+# License
+
+Raw Line Edit is released under the MIT license.
+
+Copyright (c) 2013 Isaac Muse <isaacmuse@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ raw_line_edit.py
@@ -0,0 +1,147 @@
+"""
+Raw Line Edit
+Licensed under MIT
+Copyright (c) 2013 Isaac Muse <isaacmuse@gmail.com>
+"""
+import sublime
+import sublime_plugin
+import codecs
+from os.path import basename
+
+import re
+
+new_line = "¬"
+
+
+def strip_newline_glyph(text):
+    return re.sub(r"%s\n" % new_line, "\n", text)
+
+
+def add_newline_glyph(text):
+    return re.sub(r"\n", "%s\n" % new_line, text)
+
+
+def strip_carriage_returns(text, glyph):
+    new_line_glyph = new_line if glyph else ""
+    return re.sub(r"\r*%s\n" % new_line_glyph, "%s\n" % new_line_glyph, text)
+
+
+def add_carriage_returns(text, glyph):
+    new_line_glyph = new_line if glyph else ""
+    return re.sub(r"(?<!\r)%s\n" % new_line_glyph, "\r%s\n" % new_line_glyph, text)
+
+
+class ToggleRawLineEditCommand(sublime_plugin.TextCommand):
+    def use_newline_glyph(self):
+        return bool(sublime.load_settings("raw_line_edit.sublime-settings").get("use_newline_glyph", False))
+
+    def use_theme(self):
+        return bool(sublime.load_settings("raw_line_edit.sublime-settings").get("use_raw_line_edit_theme", False))
+
+    def disable_rle(self, edit, file_name):
+        if self.view.is_dirty():
+            if sublime.ok_cancel_dialog("Raw Line Edit:\nFile has unsaved changes.  Save?"):
+                self.view.run_command("save")
+        self.view.set_scratch(True)
+        file_name = self.view.settings().get("RawLineEditFilename")
+        syntax = self.view.settings().get("RawLineEditSyntax")
+        win = self.view.window()
+        temp = None
+        if len(win.views()) <= 1:
+            temp = win.new_file()
+        win.focus_view(self.view)
+        win.run_command("close_file")
+        new_view = win.open_file(file_name)
+        if temp is not None:
+            win.focus_view(temp)
+            win.run_command("close_file")
+        win.focus_view(new_view)
+        new_view.set_syntax_file(syntax)
+
+    def enable_rle(self, edit, file_name):
+        if self.view.is_dirty():
+            if sublime.ok_cancel_dialog("Raw Line Edit:\nFile has unsaved changes.  Save?"):
+                self.view.run_command("save")
+        with codecs.open(file_name, "r", "utf-8") as f:
+            use_glyph = self.use_newline_glyph()
+            if use_glyph:
+                self.view.replace(edit, sublime.Region(0, self.view.size()), add_newline_glyph(f.read()))
+            else:
+                self.view.replace(edit, sublime.Region(0, self.view.size()), f.read())
+            self.view.set_line_endings("Unix")
+            self.view.settings().set("RawLineEditGlyph", use_glyph)
+            self.view.settings().set("RawLineEdit", True)
+            self.view.settings().set("RawLineEditSyntax", self.view.settings().get('syntax'))
+            self.view.settings().set("RawLineEditFilename", file_name)
+            if self.use_theme():
+                self.view.set_syntax_file("Packages/RawLineEdit/RawLineEdit.hidden-tmLanguage")
+            self.view.set_scratch(True)
+            self.view.set_read_only(True)
+
+    def run(self, edit):
+        file_name = self.view.file_name()
+        if file_name is not None or self.view.settings().get("RawLineEdit", False):
+            if self.view.settings().get("RawLineEdit", False):
+                self.disable_rle(edit, file_name)
+            else:
+                self.enable_rle(edit, file_name)
+
+
+class RawLineInsertCommand(sublime_plugin.TextCommand):
+    def run(self, edit, style="Unix"):
+        self.view.set_scratch(False)
+        self.view.set_read_only(False)
+        use_glyph = self.view.settings().get("RawLineEditGlyph")
+        for s in reversed(self.view.sel()):
+            line_region = self.view.full_line(s)
+            if style == "Unix":
+                self.view.replace(edit, line_region, strip_carriage_returns(self.view.substr(line_region), use_glyph))
+            else:
+                self.view.replace(edit, line_region, add_carriage_returns(self.view.substr(line_region), use_glyph))
+        self.view.set_read_only(True)
+
+
+class RawLinesEditReplaceCommand(sublime_plugin.TextCommand):
+    text = None
+    region = None
+
+    def run(self, edit):
+        cls = RawLinesEditReplaceCommand
+        if cls.text is not None and cls.region is not None:
+            self.view.replace(edit, cls.region, cls.text)
+        cls.text = None
+        cls.region = None
+
+
+class RawLineEditListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if view.settings().get("RawLineEdit", False):
+            use_glyph = view.settings().get("RawLineEditGlyph")
+            RawLinesEditReplaceCommand.region = sublime.Region(0, view.size())
+            if use_glyph:
+                RawLinesEditReplaceCommand.text = strip_newline_glyph(view.substr(RawLinesEditReplaceCommand.region))
+            else:
+                RawLinesEditReplaceCommand.text = view.substr(RawLinesEditReplaceCommand.region)
+            view.set_read_only(False)
+            view.run_command("raw_lines_edit_replace")
+            view.set_read_only(True)
+
+    def on_post_save(self, view):
+        if view.settings().get("RawLineEdit", False):
+            use_glyph = view.settings().get("RawLineEditGlyph")
+            RawLinesEditReplaceCommand.region = sublime.Region(0, view.size())
+            if use_glyph:
+                RawLinesEditReplaceCommand.text = add_newline_glyph(view.substr(RawLinesEditReplaceCommand.region))
+            else:
+                RawLinesEditReplaceCommand.text = view.substr(RawLinesEditReplaceCommand.region)
+            view.set_read_only(False)
+            view.run_command("raw_lines_edit_replace")
+            view.set_scratch(True)
+            view.set_read_only(True)
+            view.set_syntax_file("Packages/RawLineEdit/RawLineEdit.hidden-tmLanguage")
+
+    def on_query_context(self, view, key, operator, operand, match_all):
+        handeled = False
+        if view.settings().get("RawLineEdit", False) and key.startswith("raw_line_edit"):
+            handeled = True
+        return handeled
